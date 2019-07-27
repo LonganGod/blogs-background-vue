031ab915edcff7ced987adde133afe8a258947ca
LonganGod <ws15531085321@163.com>
2019-07-27 15:58:08 +0800

    登录

--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import {Input, Button, Row, Col} from 'element-ui'
+
+import 'element-ui/lib/theme-chalk/index.css';
+import './assets/less/default/base.less';
+import './assets/less/default/default.less';
 
 Vue.config.productionTip = false
+
+Vue.use(Input)
+Vue.use(Button)
+Vue.use(Row)
+Vue.use(Col)
 
 /* eslint-disable no-new */
 new Vue({
--- src/views/login/login.vue
+++ src/views/login/login.vue
@@ -1,15 +1,96 @@
 <template>
-  <div>
-    aaaaa
+  <div class="loginMainBG">
+    <div class="loginMainBox">
+      <div class="clearfix">
+        <img class="left" src="../../assets/images/dzgj-logo.png" alt="">
+        <h3 class="right title">管理员登录</h3>
+      </div>
+      <div class="adminInfoBox">
+        <el-input
+          type="text"
+          placeholder="请输入用户名"
+          prefix-icon="el-icon-user"
+          v-model="userName"
+          clearable
+        >
+        </el-input>
+        <el-input
+          type="password"
+          placeholder="请输入密码"
+          prefix-icon="el-icon-lock"
+          v-model="password"
+          clearable
+          show-password
+        >
+        </el-input>
+        <el-button type="primary">登&emsp;录</el-button>
+        <div class="clearfix others">
+          <span class="left">忘记密码？</span>
+          <span class="right" @click="clearInfo">重置</span>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
   export default {
-    name: "login"
+    name: "login",
+    data() {
+      return {
+        userName: 'admin',
+        password: 'admin',
+      }
+    },
+    methods: {
+      clearInfo() {
+        this.userName = ''
+        this.password = ''
+      }
+    }
   }
 </script>
 
-<style scoped>
+<style scoped lang="less">
+  .loginMainBG {
+    position: fixed;
+    width: 100%;
+    height: 100%;
+    background-color: #2f4157;
 
+    .loginMainBox {
+      width: 450px;
+      /*height: 400px;*/
+      /*background-color: #fff;*/
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      transform: translate(-50%, -70%);
+    }
+
+    .title {
+      font-size: 20px;
+      font-weight: 400;
+      color: #FFF;
+      line-height: 40px;
+    }
+
+    .el-input, .el-button {
+      margin-top: 30px;
+    }
+
+    .el-button {
+      width: 100%;
+    }
+
+    .others {
+      margin-top: 10px;
+      padding: 0 10px;
+      color: #ccc;
+
+      span {
+        cursor: pointer;
+      }
+    }
+  }
 </style>
