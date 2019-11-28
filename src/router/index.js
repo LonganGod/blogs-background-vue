8871357c268c670121a07b55d3bb12694755e5b0
wangshuo <ws15531085321@163.com>
2019-11-28 15:43:44 +0800

    权限验证

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Default from './default/default'
+import axios from 'axios'
+import {Message} from "element-ui";
 
 Vue.use(Router)
 
@@ -14,16 +16,28 @@ const router = new Router({
   ]
 })
 
-router.beforeEach((to, from, next) => {
+router.beforeEach(async (to, from, next) => {
   if (to.path != '/login') {
     const adminId = window.sessionStorage.getItem('adminId')
     if (adminId) {
-      next();
+      let {data} = await axios.post('/api/checkPermission', {
+        adminId: window.sessionStorage.getItem('adminId'),
+        path: to.path
+      })
+
+      if (data.code == 200) {
+        next()
+      } else {
+        return Message({
+          type: 'warning',
+          message: '对不起，您没有操作权限！'
+        })
+      }
     } else {
       next({path: "/login"})
     }
   } else {
-    next();
+    next()
   }
 });
 
